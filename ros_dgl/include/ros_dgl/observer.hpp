cb598c4bd1f4cb8c742897a8f8ccb3b24613bb3a
Sebastian Peralta <peraltas@seas.upenn.edu>
2023-11-20 09:50:31 +0000

    generic

--- ros_dgl/include/ros_dgl/observer.hpp
+++ ros_dgl/include/ros_dgl/observer.hpp
@@ -11,7 +11,7 @@
 
 namespace ros_dgl
 {
-template <typename ObsT, typename... SrcTs>
+template <typename ObsT, typename SrcT>
 
 /**
  * @brief ROS params: src_topic, publish=false
@@ -21,15 +21,10 @@ class Observer : public rclcpp::Node
 {
 public:
   // Set up parent Node.
-  Observer(const rclcpp::NodeOptions& options, std::initializer_list<std::string> src_topics)
-    : Node("observer", options)
+  Observer(const rclcpp::NodeOptions& options) : Node("observer", options)
   {
-    int i = 0;
-    for(const auto& src_topic : src_topics) {
-    this->declare_parameter("src_topics" + std::to_string( i), src_topic);
-    i++;
-    }
-    this->declare_parameter("publish", true);
+      this->declare_parameter("src_topic", "");
+      this->declare_parameter("publish", true);
   }
   /**
    * @brief  Observer subscribes to src_topic and stores
@@ -38,14 +33,12 @@ public:
    * @param options NodeOptions for the Node.
    * @param obs_from_src Function that creates Observation from Source Data
    */
-  Observer(const rclcpp::NodeOptions& options, std::function<std::unique_ptr<ObsT>(const SrcTs&...)> obs_from_srcs,
-          std::initializer_list<std::string> src_topics);
+  Observer(const rclcpp::NodeOptions& options, std::function<std::unique_ptr<ObsT>(const SrcT&)> obs_from_src);
 
   ObsT* getObservation()
   {
-    observation_ = std::move(std::apply(obs_from_srcs_, src_msgs_));
     std::shared_lock lock(obs_mutex_);
-    return observation_.get();
+    return last_observation_.get();
   }
 
   std::shared_lock<std::shared_mutex> getSharedLock() const
@@ -56,11 +49,9 @@ public:
 
 private:
   typename rclcpp::Publisher<ObsT>::SharedPtr obs_pub_;
-  std::tuple<typename rclcpp::Subscription<SrcTs>::SharedPtr...> src_subs_;
-  std::tuple<std::unique_ptr<SrcTs>...> src_msgs_;
-  mutable std::shared_mutex obs_mutex_;
-  std::unique_ptr<ObsT> observation_;
+  typename rclcpp::Subscription<SrcT>::SharedPtr src_sub_;
 
-  std::function<std::unique_ptr<ObsT>(const SrcTs&...)> obs_from_srcs_;
+  mutable std::shared_mutex obs_mutex_;
+  std::unique_ptr<ObsT> last_observation_ RCPPUTILS_TSA_GUARDED_BY(obs_mutex_);
 };
 }  // namespace ros_dgl
